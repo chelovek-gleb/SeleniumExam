2e46e62de767bd64b6a1a6761b8bec4d260b9e57
Gleb <shufllin@gmail.com>
2025-08-18 17:43:33 +0400

    добавил вывод размера окна

--- tests/test_name_and_price_in_cart.py
+++ tests/test_name_and_price_in_cart.py
@@ -18,7 +18,7 @@ def test_perehod_catalog(browser):
     products_page = ProductsPage(browser)
     cart_page = CartPage(browser)
     base = Base(browser)
-    print(f'Размер окна браузера: {browser.get_window_size()}')
+    print(f'Размер окна браузера: {browser.get_window_size()}') # принт
 
 
     main_page.catalog() #Переходим в каталог
